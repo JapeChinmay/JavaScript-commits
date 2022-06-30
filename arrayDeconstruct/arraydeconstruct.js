1ba20b6e48ee323fb1ecbdd93abee30ae8f90609
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-06-30 22:11:52 +0530

    Add files via upload

---
+++ arrayDeconstruct/arraydeconstruct.js
@@ -0,0 +1,130 @@
+"use strict";
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (startIndex, mainIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function (startIndex, mainIndex, time, address) {
+    console.log(`Order reached , ${this.starterMenu[startIndex]}, and
+    ${this.mainMenu[mainIndex]},will be delivered to ${address}, at ${time}
+    `);
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+restaurant.orderDelivery({
+  startIndex: 1,
+  mainIndex: 2,
+  time: "22.22",
+  address: "jalna",
+});
+
+///////////////////////////////////////
+
+const chinmay = {
+  firstName: "chinmay",
+  location1: "aurangabad",
+  job: "software engg",
+  genger: "Male",
+
+  workTime: {
+    mon: {
+      startingtime: 1,
+      closingtime: 4,
+    },
+    tue: {
+      startingtime: 1,
+      closingtime: 4,
+    },
+    wed: {
+      startingtime: 1,
+      closingtime: 4,
+    },
+  },
+};
+
+const { firstName, location1, job, gender } = chinmay;
+console.log(firstName, location1, job, gender);
+
+const { firstName: petName, location1: home, gender: maleorfemale } = chinmay;
+console.log(petName, home, maleorfemale);
+
+///////////////////////////////////////////////////////////////////////////
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//// destructuring object in an object and assign them new notations
+const {
+  fri: { open: o, close: c1 },
+} = openingHours;
+console.log(o, c1);
+
+//mutating
+let a1 = 1111;
+let b1 = 5656;
+const obj = { a: 23, b: 7, c: 56 };
+
+({ a1, b1 } = obj);
+console.log(a1, b1);
+
+///////////////////////////////////////////////////
+//manual way
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+//destructuring
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, secondary] = restaurant.categories;
+//skip middle element if u want diff combo
+
+console.log(main, secondary);
+// switchingvalues
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// got the values from function
+const [starter, main1] = restaurant.order(2, 2);
+console.log(starter, main1);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, t]] = nested;
+console.log(i, j, t);
