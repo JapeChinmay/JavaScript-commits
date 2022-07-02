334fd56d06ab5b7888d12ddde0f50fe462b5dcf0
Chinmay Jape <107535441+JapeChinmay@users.noreply.github.com>
2022-07-02 19:45:30 +0530

    Add files via upload

--- functions(advanced)/functionsad.js
+++ functions(advanced)/functionsad.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const bookings = [];
+/*const bookings = [];
 
 const createBookings = function (flightNum, numPass, price) {
   const booking = {
@@ -16,6 +16,7 @@ createBookings("KKFKF", "1211212", "1211212");
 createBookings("dffd", "2121212", "32323");
 
 console.log(...bookings);
+*/
 
 ////////////////////////////////
 
@@ -34,3 +35,43 @@ const transformer = function (str, fn, fn2) {
 };
 
 transformer("chinmay is jape", upperFirstWord, oneWord);
+
+///////////functions returning functions///
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterWelcome = greet("welcome");
+greeterWelcome("chinmay");
+
+////call and apply method//
+
+const IndianAirways = {
+  airline: "Indian Airways",
+  iataCode: "IA",
+  bookkings: [],
+  book: function (flightNum, name) {
+    console.log(
+      `${name} booked seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookkings.push({ flight: ` ${this.iataCode}${flightNum}`, name });
+  },
+};
+
+IndianAirways.book("11212", "chinmay");
+IndianAirways.book("ddsd", "jape");
+
+const hindAirways = {
+  airline: "hindAirways",
+  iataCode: "hind",
+  bookkings: [],
+};
+
+const book = IndianAirways.book;
+book.call(hindAirways, "223", "chinmay");
+book.call(IndianAirways, "344", "fdf");
+
+console.log(IndianAirways);
